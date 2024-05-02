5103d9ce45a31029f6ebee287c4dfb9ecad3eb0f
shreyash776 <shreyashchaurasia77@gmail.com>
2024-05-02 13:28:37 +0530

    added new projects

--- portfolio/src/Components/Projects.jsx
+++ portfolio/src/Components/Projects.jsx
@@ -3,27 +3,43 @@ import {motion,useScroll,useSpring, useTransform} from "framer-motion"
 const items=[
     {
         id:1,
+        title:"WordCraft",
+        img:"/portfolio/public/Screenshot 2024-05-02 130732.png",
+       git:"https://github.com/shreyash776/blog-backend",
+        desc:
+        "A dynamic blogging website designed to empower users to express themselves through writing. This platform offers a seamless experience for both writers and readers, allowing users to create, read, edit, and delete their blogs effortlessly. With a user-friendly interface, individuals can explore a variety of topics, engage with thought-provoking content, and interact with a vibrant community through comments and discussions. "
+    },
+    {
+        id:2,
+        title:"Dashboard",
+        img:"/dashboard.png",
+       git:"https://github.com/shreyash776/Dashboard",
+        desc:
+        "Introducing our MERN Stack Dashboard: Dynamic graphs, customizable filters. Real-time insights."
+    },
+    {
+        id:3,
         title:"Nike Store",
         desc:"This project is an e-commerce website developed using React.js, offering a user-friendly interface for browsing and purchasing premium footwear. ",
         img:"/nike.png",
         git:"https://github.com/shreyash776/Nike-store"
     },
     {
-        id:2,
+        id:4,
         title:"Plant vs Zombie",
         desc:"It is a JavaScript and HTML Canvas game reminiscent of Plants vs. Zombies. Strategically deploy plants to fend off waves of zombies in this addictive, fast-paced arcade experience. ",
         img:"/Screenshot 2024-01-25 201551.png",
         git:"https://github.com/shreyash776/plant-vs-zombie"
     },
     {
-        id:3,
+        id:5,
         title:"Todo-list",
         img:"/todo.png ",
         desc:  "React ToDo is a minimalist to-do list app crafted with React.js. Effortlessly manage tasks with streamlined features for quick adding, deleting, and marking as complete. Stay organized and focused with ReactToDo's clean interface and lightning-fast performance.",
         git:"https://github.com/shreyash776/Todo-list"
     },
     {
-        id:4,
+        id:6,
         title:"Amazon-clone",
         img:"/amazon.png",
       
@@ -32,7 +48,7 @@ const items=[
     },
  
     {
-        id:5,
+        id:7,
         title:"Traveler",
         img:"/traveller.png",
       
@@ -43,20 +59,21 @@ const items=[
 
 
     {
-        id:6,
+        id:8,
         title:"Weather",
         img:"/weather.png",
        git:"https://github.com/shreyash776/weather_app",
         desc:"A modern weather app powered by React, delivering real-time forecasts and intuitive user experience for weather updates."
     },
     {
-        id:7,
+        id:9,
         title:"Js Animations",
         img:"/animation.png",
        git:"https://github.com/shreyash776/Animation",
         desc:
         "A captivating web page enhanced with JavaScript animations, offering an engaging user experience through dynamic visual effects and interactivity."
     },
+    
 ]
 
 const Single=({item})=>{
